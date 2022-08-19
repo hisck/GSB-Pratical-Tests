f70535375f954e2cf5d8e98c26406fc8c1aa42fc
hisck <ra99829@uem.br>
2022-08-19 01:50:30 -0300

    Last try to fix 502 error on aws

--- Application/crud-rest-api/src/main/java/mateussantos/crud/rest/api/controller/UserController.java
+++ Application/crud-rest-api/src/main/java/mateussantos/crud/rest/api/controller/UserController.java
@@ -1,12 +1,12 @@
 package mateussantos.crud.rest.api.controller;
 
+import com.google.gson.Gson;
 import mateussantos.crud.rest.api.model.UserModel;
 import mateussantos.crud.rest.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.google.gson.Gson;
 
 import java.util.List;
 
